024c70723e3da4c962c7e60beeec6a7c4ef99682
jkitchens6057 <jkitchens6057@floridapoly.edu>
2026-01-12 19:26:07 +0000

    Added LO1 homework file: print_test.py

---
+++ print_test.py
@@ -0,0 +1,6 @@
+name = "Jacob Kitchens"
+
+print("Name: " + name)
+print("Use 4 spaces per indentation level.") 
+print("Python does not use semicolons to end statements")
+print("In this course, I hope to reacquaint myself with Python, gain a better understanding of scripting languages and their utility, and learn to think in a way conductive to problem solving.")
