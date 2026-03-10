6abaecc8a55b2b6d2b6a0bf3a99f8c305e4936d4
jkitchens6057 <jkitchens6057@floridapoly.edu>
2026-03-10 02:01:26 +0000

    Added L19 and L21 Paricipatiion Files

---
+++ L21/numpy_df_streamlit.py
@@ -0,0 +1,41 @@
+# numpy_random.py
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.write("numpy_df_streamlit.py")
+num_rows = st.slider("Number of rows", 1, 10000, 500)
+np.random.seed(np.random.randint(2,50))
+
+num_rows = 5
+data = []
+for i in range(num_rows):
+    data.append(
+        {
+            "Preview":
+            f"https://picsum.photos/400/200?lock={i}",
+            "Views": np.random.randint(0, 1000),
+            "Active": np.random.choice([True, False]),
+            "Category": np.random.choice(["￿ LLM", "￿ Data",
+            "⚙️ Tool"]),
+            "Progress": np.random.randint(1, 100),
+        }
+    )
+
+
+
+### continuation of numpy_random.py
+data = pd.DataFrame(data)
+
+st.subheader("Top 10 Views")
+st.line_chart(data["Views"].head(10))
+
+
+config = {
+    "Preview": st.column_config.ImageColumn(),
+    "Progress": st.column_config.ProgressColumn(),
+}
+if st.toggle("Enable editing"):
+        edited_data = st.data_editor(data, column_config=config, use_container_width=True)
+else:
+    st.dataframe(data, column_config=config, use_container_width=True)
